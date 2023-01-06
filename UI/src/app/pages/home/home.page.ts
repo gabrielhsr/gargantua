c1cb82f88bf0c420d9e75396f924c7b5383dc5d0
Gabriel Henrique da Silva Ribeiro <gabriel_hsr@live.com>
2023-01-05 21:54:00 -0300

    Started homepage.

--- UI/src/app/pages/home/home.page.ts
+++ UI/src/app/pages/home/home.page.ts
@@ -1,11 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { combineLatest, of, Subject, switchMap } from 'rxjs';
+import { Period } from 'src/app/entities/period/period.dto';
+import { ExpenseService } from '../expenses/services/expense.service';
+import { RevenueService } from '../revenue/services/revenue.service';
 @Component({
 	selector: 'page-home',
 	templateUrl: './home.page.html',
 	styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-	constructor() {}
+	public periodSubject = new Subject<Period | undefined>();
 
-	public ngOnInit(): void {}
+	public totalRevenue: number = 0;
+	public totalExpenses: number = 0;
+
+	constructor(
+		private readonly revenueService: RevenueService,
+		private readonly expensesService: ExpenseService
+	) {}
+
+	public ngOnInit(): void {
+		this.periodSubject
+			.pipe(switchMap((period) => {
+				if (period) {
+					const revenue = this.revenueService.getRevenueByPeriod(period);
+					const expenses = this.expensesService.getExpensesByPeriod(period);
+
+					return combineLatest({ revenue, expenses });
+				}
+
+
+				return of(period);
+			}))
+			.subscribe((res) => {
+				const expenses = res?.expenses;
+				const revenue = res?.revenue;
+
+				if (expenses?.isSuccess) {
+					this.totalExpenses = expenses.value.map((x) => x.amount ?? 0).reduce((acc, val) => acc + val, 0);
+				}
+
+				if (revenue?.isSuccess) {
+					this.totalRevenue = revenue.value.map((x) => x.amount ?? 0).reduce((acc, val) => acc + val, 0);
+				}
+			});
+	}
 }
