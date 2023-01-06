58e6b35063405cee9e573f0529c819acfe68c26c
Gabriel Henrique da Silva Ribeiro <gabriel_hsr@live.com>
2023-01-05 21:28:36 -0300

    Added dynamic title to header.

--- UI/src/app/shared/components/topbar/topbar.component.ts
+++ UI/src/app/shared/components/topbar/topbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { capitalize } from '../../helpers/string.helper';
 import { TranslateService } from '../../translate/translate.service';
 
 @Component({
@@ -8,8 +10,22 @@ import { TranslateService } from '../../translate/translate.service';
 })
 export class TopbarComponent {
 	@Output() public sidebarButton = new EventEmitter();
+	public title: string;
 
-	constructor(private readonly translate: TranslateService) {}
+	constructor(
+		private readonly translate: TranslateService,
+		private readonly router: Router
+	) {
+		this.title = this.translate.instant('Title');
+
+		this.router.events.subscribe(nav => {
+			if (nav instanceof NavigationEnd) {
+				const routeName = capitalize(nav.url.replace('/', ''));
+
+				this.title = this.translate.instant(`Sidebar.${routeName}`);
+			}
+		});
+	}
 
 	public changeLanguage(): void {
 		if (this.translate.language === 'en-US') {
