7a4123d432fbafa20effd88a159168c7002e819e
Gabriel Henrique <gabriel_hsr@live.com>
2023-02-05 11:38:05 -0300

    Updated configs and app name.

--- UI/karma.conf.js
+++ UI/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/financial'),
+      dir: require('path').join(__dirname, './coverage/gargantua'),
       subdir: '.',
       reporters: [
         { type: 'html' },
